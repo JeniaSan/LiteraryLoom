28783fb1805f5f9950cd14ed4cc57ab676a1fc43
Evhenii Polishuchenko <sds191sds@gmail.com>
2023-09-21 17:51:59 +0300

    added docker to project

--- src/test/java/com/polishuchenko/bookstore/BookStoreApplicationTests.java
+++ src/test/java/com/polishuchenko/bookstore/BookStoreApplicationTests.java
@@ -1,11 +1,8 @@
 package com.polishuchenko.bookstore;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BookStoreApplicationTests {
-    @Test
-    void contextLoads() {
-    }
+
 }
